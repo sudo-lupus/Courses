70ed689acf1c79401efdce1eb74dc7bc89284ae9
sudo-lupus <67869327+sudo-lupus@users.noreply.github.com>
2023-03-07 17:25:44 +0100

    Clean up file structure

---
+++ coursera_google_it_automation_with_python/python_crash_course/introduction_to_python.py
@@ -0,0 +1,10 @@
+friends = ['Taylor', 'Alex', 'Pat', 'Eli']
+for friend in friends:
+    print("Hi " + friend)
+
+#Print Hello, World! 10 times
+for i in range(10):
+    print("Hello, World!")
+
+name = "Brook"
+print(f"Hello, {name}")
