28fbe42d184bae97c5e5c8bd4b67266e171cfde2
sudo-lupus <67869327+sudo-lupus@users.noreply.github.com>
2023-03-10 05:28:41 +0100

    Finished own practice project

--- coursera_google_it_automation_with_python/python_operating_system/regular_expressions/regex_project.py
+++ coursera_google_it_automation_with_python/python_operating_system/regular_expressions/regex_project.py
@@ -1,7 +1,27 @@
 import csv
 import re
 
-with open("log_file.csv") as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        print(row)
+
+with open("log_file.csv","r") as f:
+    text = f.read()
+
+new_text = re.sub(r";", r",", text)
+new_text = re.sub(r" ", r"_", new_text)
+
+f2 = open("log_file_2.csv", "w")
+f2.write(new_text)
+f2.close()
+
+manchester_file = "manchester.csv"
+f_manchester = open(manchester_file,"w")
+
+with open("log_file_2.csv") as f:
+    csv_f = csv.DictReader(f)
+    csv_writer = csv.DictWriter(f_manchester, fieldnames = csv_f.fieldnames)
+    csv_writer.writeheader()
+    for row in csv_f:
+        match = re.match(r"Manchester$", row["Location"])
+        if match != None:
+            csv_writer.writerow(row)
+
+f_manchester.close()
