e00d9c2d800e5142b04e8c3ba47b6e2a2d636873
sudo-lupus <67869327+sudo-lupus@users.noreply.github.com>
2023-03-16 18:53:52 +0100

    Add Bash Scripts

---
+++ coursera_google_it_automation_with_python/python_operating_system/week6_bash_scripting/bash_scripts/gather-information.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+line="-----------------------------------"
+echo "Starting at: $(date)"; echo $line
+
+echo "UPTIME"; uptime; echo $line
+
+echo "FREE"; free; echo $line
+
+echo "WHO"; whoami; echo $line
+
+# echo "Public IP"; curl icanhazip.com; echo $line
+
+echo "Finishing at: $(date)"
